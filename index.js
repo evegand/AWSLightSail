76a26289e839d76de3f59b3c39c11ca4d7f08d20
evegand <a19310213@ceti.mx>
2023-02-28 12:43:41 -0600

    New Changes

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
-const express = require("express");
+const express = require("express"); // constante con libreria de express
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.listen(port, () => {
-    console.log(`Example app listening at http://localost:${port}`);
+app.use(express.json()); // Librerias de json
+
+const students = [
+    {id: 1, name: 'Jair', age: 23, active: true},
+    {id: 2, name: 'Erubey', age: 23, active: true},
+    {id: 3, name: 'Mariana', age: 23, active: true},
+    {id: 4, name: 'Eve', age: 25, active: true}
+]
+
+/** Home page */
+app.get('/', (req, res) => {
+    res.send('Node JS api');
 });
 
+/** Mostrar estudiantes */
+app.get('api/students', (req, res) => {
+    res.send(students);
+});
+
+/** Buscar estudiante por ID */
+app.get('/api/students/:id', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if (!student) return res.status(404).send("Estudiante no encontrado");
+});
+
+/** Agregar estudiante */
+app.post('/api/students', (req, res) => {
+    const student = {
+        id: students.length + 1,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        active: (req.body.active === 'true')
+    };
+
+    students.push(student);
+    res.send(student);
+});
+
+/** Eliminar estudiante */
+app.delete('api/students/:id', (req, res) => {
+    const student = students.find(c => c.id === parseInt(req.params.id));
+    if (!student) return res.status(404).send('Estudiante no encontrado');
+
+    const index = students.indexOf(student);
+    index.splice(index, 1);
+    res.send(student);
+})
+
+/** Listen */
+app.listen(port, () => {
+    console.log(`Escuchando en http://localost:${port}`);
+});
+
+/*
 app.get("/", (req, res) => {
     res.send(`Hello ${port}`);
 });
@@ -14,3 +63,4 @@ app.get("/", (req, res) => {
 app.get("/:name", (req, res) => {
     res.send(`Hello ${req.params.name}`);
 });
+*/
